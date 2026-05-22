9047d909d195969082c70c2864fb9ca6631682fd
Tynchtykbek <tyncha.leo@gmail.com>
2026-05-22 15:34:38 +0200

    Add basic text analysis functions

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_characters(text):
+    return len(text)
+
+
 def main():
-    print("AI Text Analyzer API project started.")
+    user_text = input("Enter text to analyze: ")
+
+    word_count = count_words(user_text)
+    character_count = count_characters(user_text)
+
+    print("Analysis result:")
+    print(f"Words: {word_count}")
+    print(f"Characters: {character_count}")
 
 
 if __name__ == "__main__":
